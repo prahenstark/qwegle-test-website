8500cab51559385ea136cec0b1ec77b51efe832e
prahenstark <parijaprahen@gmail.com>
2024-01-13 12:16:16 +0530

    13th jan update

--- src/App.js
+++ src/App.js
@@ -1,25 +1,86 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import './style/App.scss';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// Components
+import DesktopNav from './components/navbar/desktop-nav';
+import MobileNav from './components/navbar/mobile-nav';
+import Backdrop from "./components/navbar/backdrop";
+import Hero1 from './components/hero1/hero1';
+import Hero2 from './components/hero2/hero2'
+import Portfolio from "./components/portfolio/portfolio";
+import Partners from "./components/partners/partners";
+import About from "./components/about/about";
+import Testimonial from "./components/blog/blog";
+import Contact from "./components/contact/contact";
+import Footer from "./components/footer/footer";
+ 
+class App extends React.Component {
+  state = {
+    userIsScrolled: false,
+    mobileNavbarOpen: false,
+  };
+
+  componentDidMount() {
+    window.addEventListener("scroll", this.userIsScrolled);
+  }
+  componentWillUnmount() {
+    window.removeEventListener("scroll", this.userIsScrolled);
+  }
+
+  // Detect if user is scorlled down (used for add/disable extra large navbar)
+  userIsScrolled = () => {
+    if (window.pageYOffset > 80) {
+      this.setState({ userIsScrolled: true });
+    } else {
+      this.setState({ userIsScrolled: false });
+    }
+  };
+  // On closeMobileMenu click close navbar
+  closeMobileMenu = () => {
+    this.setState({ mobileNavbarOpen: false });
+  };
+  // Mobile menu handler
+  mobileMenuOpen = () => {
+    this.setState({ mobileNavbarOpen: true });
+  };
+
+  render() {
+    // BACKDROP RENDER
+    let backdrop = <Backdrop closeMobileMenu={this.closeMobileMenu} />;
+    if (this.state.mobileNavbarOpen) {
+      backdrop = (
+        <Backdrop closeMobileMenu={this.closeMobileMenu} isOpen={true} />
+      );
+    }
+    // MOBILE NAVBAR RENDER
+    let mobileNavbar = <MobileNav />;
+    if (this.state.mobileNavbarOpen) {
+      mobileNavbar = (
+        <MobileNav isOpen={true} closeMobileMenu={this.closeMobileMenu} />
+      );
+    }
+
+    return (
+      <div className="App">
+        {mobileNavbar}
+        {backdrop}
+        <DesktopNav
+          userIsScrolled={this.state.userIsScrolled}
+          mobileMenuOpen={this.mobileMenuOpen}
+        />
+        <Hero1 />
+        <Hero2/>
+        <Hero1 />
+        <Hero2/>
+        {/* <Portfolio /> */}
+        {/* <Partners />
+        <About /> */}
+        <Testimonial />
+        <Contact />
+        <Footer />
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/hero1/hero1.jsx
@@ -0,0 +1,73 @@
+import React from "react";
+import { Row, Col } from "react-flexbox-grid";
+// SCSS
+import "./hero1.scss";
+import 'swiper/scss';
+import 'swiper/scss/navigation';
+import 'swiper/scss/pagination';
+//Assets
+import HeroImage from '../../assets/hero/qwegle image.jpg';
+// import HeroImage from '../../assets/hero/hero-image.png';
+//Components
+import Button from '../ui-components/button/button';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import {Pagination, EffectCards, Autoplay } from 'swiper/modules';
+
+
+
+const hero1 = () => {
+  
+  return (
+  <div className="hero1" id="hero1">
+    <div className="wrapper">
+      <Row>
+        <Col md={12} lg={6}>
+          <div className="hero-info">
+            <h1 className="weight800 font60">Direct </h1>
+            <h1 className="weight800 font60">
+              From Tap Policy
+            </h1>
+            <p className="font12">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+              doeiusmod tempor incididunt ut labore et dolore magna aliqua
+            </p>
+            <Button label="LEARN MORE" target={"contact"} />
+          </div>
+        </Col>
+        <Col md={12} lg={6}>
+          <Swiper 
+          className="swiper1"
+            modules={[Pagination, Autoplay, EffectCards]}
+            slidesPerView={1}
+            pagination={{ clickable: true }}
+            autoplay={{delay: 4000}}
+            effect="cards"
+            
+            >
+
+            <SwiperSlide className="swiper1-slider">
+              <img className="hero-image" src={HeroImage} alt="hero" />
+            </SwiperSlide>
+            <SwiperSlide >
+              <img className="hero-image" src={HeroImage} alt="hero" />
+            </SwiperSlide>
+            <SwiperSlide >
+              <img className="hero-image" src={HeroImage} alt="hero" />
+            </SwiperSlide>
+            <SwiperSlide >
+              <img className="hero-image" src={HeroImage} alt="hero" />
+            </SwiperSlide>
+            <SwiperSlide >
+              <img className="hero-image" src={HeroImage} alt="hero" />
+            </SwiperSlide>
+            <SwiperSlide >
+              <img className="hero-image" src={HeroImage} alt="hero" />
+            </SwiperSlide>
+          </Swiper>
+        </Col>
+      </Row>
+    </div>
+  </div>
+  );
+};
+export default hero1;
